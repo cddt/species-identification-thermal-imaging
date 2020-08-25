954d18a0731817daeeb1f58f2973d265a31a50be
cddt <35991372+cddt@users.noreply.github.com>
2020-08-25 21:47:57 +1200

    Update Statistical analysis.py

--- preprocess/Statistical analysis.py
+++ preprocess/Statistical analysis.py
@@ -10,12 +10,24 @@ import os
 f = h5py.File("/home/cddt/data-space/Cacophony/data1/dataset.hdf5", "r") # Read in the dataset
 d = f[list(f.keys())[0]]                                        # Access the thermal videos key
 
+def get_best_index(vid):
+    """
+    Returns an index such that the selected 45 frames from a given video correspond to
+    the 45 frames where the animal is nearest to the camera.
+    """
+    mass = np.zeros(vid.attrs['frames'])
+    for f in range(vid.attrs['frames']):
+        mass[f] = np.sum(vid[str(f)][4])
+    total_mass_over_next_45 = np.cumsum(mass) - np.hstack([np.zeros(45), np.cumsum(mass[:-45])])
+    return f - np.argmax(total_mass_over_next_45[::-1]) - 44     
+
 tag_list = []
 device_list = []
 fps_list = []
 location_list = []
 frames_list = []
 start_time = []
+best_mass = {}
 for i in range(len(d.keys())):
     x = d[list(d.keys())[i]]
     device_list.append(x.attrs['device'])
@@ -33,7 +45,16 @@ for i in range(len(d.keys())):
             start_time.append(vid.attrs['start_time'])
         else:
             start_time.append('missing')
-
+        if vid.attrs['frames'] >= 45 and not vid.attrs['tag'] in ["unknown", "part", "poor tracking", "sealion"]:
+            ind = get_best_index(vid)
+            mass = np.zeros(45)
+            for f in range(45):
+                mass[f] = np.sum(vid[str(f+ind)][4])
+            if not vid.attrs['tag'] in best_mass:
+                best_mass[vid.attrs['tag']] = [mass[np.argmax(mass)]]
+            else:
+                best_mass[vid.attrs['tag']].append(mass[np.argmax(mass)])
+            
 # plot frames histogram with cut-off in red            
 %matplotlib inline
 import matplotlib.pyplot as plt
@@ -47,7 +68,14 @@ plt.show()
 
 # get mean frames
 np.mean(frames_list)
-            
+
+medians = []
+for key, value in best_mass.items():
+    temp = [key,np.median(value)]
+    medians.append(temp)
+    
+location_list = list(filter(lambda a: a != 'missing', location_list))
+
 counts_tag = dict()
 for i in tag_list:
     counts_tag[i] = counts_tag.get(i, 0) + 1
